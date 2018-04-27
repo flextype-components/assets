48cefbc3e5d81667f4c1239ce622202f9e3ac082
Awilum <awilum@msn.com>
2018-04-28 02:08:32 +0300

    General Updates

--- Assets.php
+++ Assets.php
@@ -14,5 +14,55 @@ namespace Flextype\Component\Assets;
 
 class Assets
 {
+    /**
+     * Assets files.
+     *
+     * @var array
+     */
+    protected static $assets = [];
 
+    /**
+     * Add new asset
+     *
+     * $username = Cookie::get('username');
+     *
+     * @param  string $asset_type
+     * @param  string $asset
+     * @param  string $namespace
+     * @param  int    $priority
+     * @return void
+     */
+    public static function add(string $asset_type, string $asset, string $namespace, int $priority = 1) : void
+    {
+        static::$assets[$namespace][$asset_type][$priority][] = [
+            'asset'  => $asset
+        ];
+    }
+
+    /**
+     * Get assets for current namespace and asset type
+     *
+     * $username = Cookie::get('username');
+     *
+     * @param  string $asset_type
+     * @param  string $namespace
+     * @return array
+     */
+    public static function get(string $asset_type, string $namespace) : array
+    {
+        // is there some registered $assets for current $namespace ?
+        if (isset(static::$assets[$namespace])
+            && isset(static::$assets[$namespace][$asset_type])
+            && count(static::$assets[$namespace][$asset_type]) > 0) {
+
+            // Get all $assets for given $namespace and $asset_type.
+            $assets = static::$assets[$namespace][$asset_type];
+
+            // Sort $assets by priority from low to high
+            ksort($assets);
+
+            // Return $assets
+            return $assets;
+        }
+    }
 }
